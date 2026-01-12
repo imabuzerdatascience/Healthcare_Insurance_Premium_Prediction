1ecba5551e086c82d39bb904a6bbcf51fcf09156
imabuzerdatascience <abuzerdatascience@gmail.com>
2026-01-12 21:59:04 +0545

    load the model from artifacts with help of joblib

--- Credit_card_app/predict_helper.py
+++ Credit_card_app/predict_helper.py
@@ -1,3 +1,8 @@
 import pandas as pd
 import joblib
 
+Model_young = joblib.load("Artifacts\model_young.joblib")
+Model_rest = joblib.load("Artifacts\model_rest.joblib")
+Scaler_young = joblib.load("Artifacts\scaler_young.joblib")
+Scaler_rest = joblib.load("Artifacts\scaler_rest.joblib") 
+
